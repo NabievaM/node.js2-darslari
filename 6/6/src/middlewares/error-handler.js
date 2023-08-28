2b251118d4d00680fa23bf6e4e389a5e1ac84814
NabievaM <101929794+NabievaM@users.noreply.github.com>
2023-08-28 12:30:28 +0500

    node.js2

--- 6/6/src/middlewares/error-handler.js
+++ 6/6/src/middlewares/error-handler.js
@@ -4,10 +4,10 @@ const CustomError = require("../utils/custom-error");
 
 const errorHandler = (err, req, res, next) => {
   if (err instanceof CustomError) {
-    return res.status(err.status).json({message: err.message});
+    return res.status(err.status).json({ message: err.message });
   }
 
-  res.status(500).json({message: "Internal Server error"});
+  res.status(500).json({ message: "Internal Server error" });
 };
 
 module.exports = errorHandler;
